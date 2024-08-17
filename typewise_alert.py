c181b96d12256acc3d39c739b275d2524cd96535
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-18 01:29:28 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,5 +1,7 @@
 temperature_breach_stmt=['PASSIVE_COOLING', 'HI_ACTIVE_COOLING','MED_ACTIVE_COOLING']
 temperature_breach_limits=[[0,35],[0,45],[0,40]]
+BreachType_email_msg={'TOO_LOW': 'too low','TOO_HIGH': 'too high', 'NORMAL': 'normal'}
+
 
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
@@ -12,7 +14,7 @@ def infer_breach(value, lowerLimit, upperLimit):
 def classify_temperature_breach(coolingType, temperatureInC):
   lowerLimit = 0
   upperLimit = 0
-  statement_index=temperature_breach_stmt.index(coolingType)
+  statement_index=temperature_breach_stmt.index(coolingType) # exception required
   lowerLimit=temperature_breach_limits[statement_index][0]
   upperLimit=temperature_breach_limits[statement_index][1]
   return infer_breach(temperatureInC, lowerLimit, upperLimit)
@@ -20,13 +22,14 @@ def classify_temperature_breach(coolingType, temperatureInC):
 
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType =classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
-  elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
-  else:
+  try:
+    if alertTarget == 'TO_CONTROLLER':
+      send_to_controller(breachType)
+    elif alertTarget == 'TO_EMAIL':
+      send_to_email(breachType)
+  except:
+    print("Please provide alertTarget as TO_CONTROLLER or TO_EMAIL")
     return 404
-
 
 def send_to_controller(breachType):
   header = 0xfeed
@@ -36,5 +39,8 @@ def send_to_controller(breachType):
 def send_to_email(breachType):
   recepient = "a.b@c.com"
   print(f'To: {recepient}')
-  BreachType_email_msg={'TOO_LOW': 'too low','TOO_HIGH': 'too high', 'NORMAL': 'normal'}
   print(f'Hi, the temperature is {BreachType_email_msg[breachType]}')
+  
+class AlertTargetOtherNotAllowed(Exception):
+  "Raised when alertTarget is set to a value other than TO_CONTROLLER or TO_EMAIL"
+  pass
