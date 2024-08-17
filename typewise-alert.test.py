e952dbee3e95bef93b5acee34b955d109f76b80c
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-17 21:09:46 +0530

    Update typewise-alert.test.py

--- typewise-alert.test.py
+++ typewise-alert.test.py
@@ -3,6 +3,8 @@ import typewise_alert
 
 
 class TypewiseTest(unittest.TestCase):
+  @patch('builtins.print')
+    
   def test_infers_breach_as_per_limits(self):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
     self.assertTrue(typewise_alert.infer_breach(50, 50, 100) == 'NORMAL')
@@ -18,9 +20,11 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 40) == 'NORMAL')
     self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', -2) == 'TOO_LOW')
     self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 41) == 'TOO_HIGH')
-  def test_infers_breach_and_sends_confirmation(self):
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', {'coolingType':'PASSIVE_COOLING'}, 35) == 1)
-    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', {'coolingType':'PASSIVE_COOLING'}, 35) == 1)
+  def test_infers_breach_and_sends_confirmation(self,print_mock):
+    check_and_alert('TO_CONTROLLER', {'coolingType':'PASSIVE_COOLING'}, 35)
+    print_mock.assert_called_with('0xfeed, NORMAL')
+    # self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', {'coolingType':'PASSIVE_COOLING'}, 35) == 1)
+    # self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', {'coolingType':'PASSIVE_COOLING'}, 35) == 1)
 
 if __name__ == '__main__':
   unittest.main()
