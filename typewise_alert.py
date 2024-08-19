defb9b57e89f4a26a8999823d459bc6ef7975024
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-08-19 15:52:22 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,6 +1,7 @@
 temperature_breach_stmt=['PASSIVE_COOLING', 'HI_ACTIVE_COOLING','MED_ACTIVE_COOLING']
 temperature_breach_limits=[[0,35],[0,45],[0,40]]
 BreachType_email_msg={'TOO_LOW': 'too low','TOO_HIGH': 'too high', 'NORMAL': 'normal'}
+alertTargets=['TO_CONTROLLER','TO_EMAIL']
 
 
 def infer_breach(value, lowerLimit, upperLimit):
@@ -23,24 +24,27 @@ def classify_temperature_breach(coolingType, temperatureInC):
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType =classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
   try:
-    if alertTarget == 'TO_CONTROLLER':
-        send_to_controller(breachType)
-    elif alertTarget == 'TO_EMAIL':
-        send_to_email(breachType)
+    if alertTarget in alertTargets:
+        send_to_controller_or_email(breachType,alertTargets.index(alertTarget)
     else:
       raise AlertTargetOtherNotAllowed    
   except AlertTargetOtherNotAllowed:
     print("Please provide alertTarget as TO_CONTROLLER or TO_EMAIL")
 
-def send_to_controller(breachType):
+def send_to_controller_or_email(breachType,recepient_index):
   header = 0xfeed
-  print(f'{header}, {breachType}')
-
-
-def send_to_email(breachType):
   recepient = "a.b@c.com"
-  # print(f'To: {recepient}')
-  print(f'Hi, the temperature is {BreachType_email_msg[breachType]}')
+  if recepient_index == 0:
+    print(f'{header}, {breachType}')
+  if recepient_index == 1:'
+    print(f'To: {recepient}')
+    print(f'Hi, the temperature is {BreachType_email_msg[breachType]}')
+
+
+# def send_to_email(breachType):
+#   recepient = "a.b@c.com"
+#   # print(f'To: {recepient}')
+#   print(f'Hi, the temperature is {BreachType_email_msg[breachType]}')
   
 class AlertTargetOtherNotAllowed(Exception):
   "Raised when alertTarget is set to a value other than TO_CONTROLLER or TO_EMAIL"
